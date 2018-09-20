43b461bc2ff1d3c9ad5dda26efdaeb3a1f22ebaa
phelan-cwi <43382718+phelan-cwi@users.noreply.github.com>
2018-09-20 10:41:12 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
 /*Changed the database into it's on file some files aren't functioning without including connection per page*/
 class Database {
-    private static $dsn = 'mysql:host=localhost;dbname=contact_lists';
+    private static $dsn = 'mysql:host=localhost;dbname=contact_list';
     private static $username = 'root';
     private static $password = 'Something0!';
     private static $db;
